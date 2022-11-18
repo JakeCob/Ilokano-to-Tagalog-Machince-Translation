89cdec8dd66d015cbd3b7b3fa2aa9db03f9b507b
HakobCob <rafaljacobmatthew@gmail.com>
2022-11-18 10:01:06 +0800

    Draft 11

--- bleu.py
+++ bleu.py
@@ -20,29 +20,29 @@ def bleuScore2Ref(ref,ref2, cand):
 df=open("bleu_scores.txt","w", encoding="utf-8")
 
 with open("ref.txt") as ref_file, open("cand.txt") as hyp_file: # manually saved the translation to a txt file
-   line = ref_file.readline()
+    line = ref_file.readline()
 
-   while line:
-    line1 = ref_file.readline()
-    ref = line1
+    while line:
+        line1 = ref_file.readline()
+        ref = line1
 
-    line2 = hyp_file.readline()
-    hyp = line2
+        line2 = hyp_file.readline()
+        hyp = line2
 
-    score = bleuScore(ref , hyp)
+        score = bleuScore(ref , hyp)
 
-    if score == 0:
-        exit()
+        if score == 0:
+            exit()
 
-    else:
-        print("Reference: ", ref)
-        print("Machine Translation: ", hyp)
-        print("BLEU Score: ", score)
+        else:
+            print("Reference: ", ref)
+            print("Machine Translation: ", hyp)
+            print("BLEU Score: ", score)
 
-        df.write('Reference: ' + ref)
-        df.write('Machine Translation: ' + hyp)
-        df.write('BLEU Score: ' + str(score))
-        df.write('\n\n')
+            df.write('Reference: ' + ref)
+            df.write('Machine Translation: ' + hyp)
+            df.write('BLEU Score: ' + str(score))
+            df.write('\n\n')
 
 df.close()
 ref_file.close()
