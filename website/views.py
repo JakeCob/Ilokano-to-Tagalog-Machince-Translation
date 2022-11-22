f56d0571c566559f88e8e93808c525081d7cb374
rupihongseokkie <ruffy.rupi@gmail.com>
2022-11-22 18:00:26 +0800

    sysout connect without .py file

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template, request, jsonify
+# import doc_trans_tl
 
 views = Blueprint('views', __name__)
 
+def execute():
+    sysout = 'not yet connected to .py file'
+    return sysout
+
 @views.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', sysout= execute())
