5722b6e427f1a987a3235c68efd5f600301d50a0
HakobCob <rafaljacobmatthew@gmail.com>
2022-11-22 23:26:56 +0800

    1st commit (11/22/2022)

--- website/views.py
+++ website/views.py
@@ -1,23 +1,29 @@
+import pandas as pd
 from flask import Blueprint, render_template, request, jsonify
+from doc_trans_tl import doc_trans
+from scoring import scoring_bleu
 
 views = Blueprint('views', __name__)
 
-temp_sen_list = ['sentence 1', 'sentence 2', 'sentence 3']
-temp_bleu_score = 0.5
-temp_ter_score = 0.7
+op_sen_list = []
+ave_bleu = 0
+temp_ter_score = 0
 
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        source_lang = request.form.get('source_lang')
-        expected_op = request.form.get('expected_op')
-        print(source_lang, expected_op)
-    
+        source = request.form.get('source_lang')
+        expected_op= request.form.get('expected_op')
+        dict_tl_il_result = doc_trans(source, expected_op)
+        dict_tl_il_result = pd.DataFrame(dict_tl_il_result)
+        op_sen_list = dict_tl_il_result['System Output'].tolist()
+        ave_bleu = scoring_bleu(dict_tl_il_result)
+        
     return render_template('index.html')
 
 
-@views.route('/results')
+@views.route('/results', methods=['GET', 'POST'])
 def results():
-    return render_template('results.html', temp_sen_list = temp_sen_list, temp_bleu_score = temp_bleu_score, temp_ter_score = temp_ter_score)
+    return render_template('results.html', op_sen_list = op_sen_list, ave_bleu = ave_bleu, temp_ter_score = temp_ter_score)
 
