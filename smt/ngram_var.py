a9eba9e8db351e183ae66c6d7559cc39e63576c9
HakobCob <rafaljacobmatthew@gmail.com>
2022-12-04 10:57:00 +0800

    Added Transform function in SMT

---
+++ smt/ngram_var.py
@@ -0,0 +1,20 @@
+unigram_list = []
+unigram_count_sen = []
+unigram_len = []
+
+bigram_list = []
+bigram_count_sen = []
+bigram_len = []
+
+trigram_list=[]
+trigram_count_sen=[]
+trigram_len = []
+
+fourgram_list=[]
+fourgram_count_sen=[]
+fourgram_len = []
+
+ngram_list = []
+
+notencap_list = []
+notencap_count_sen = []
