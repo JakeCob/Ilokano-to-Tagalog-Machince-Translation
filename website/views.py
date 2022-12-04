1e797c573cc64774352059e8743b2b45ec4de162
HakobCob <rafaljacobmatthew@gmail.com>
2022-12-04 17:12:58 +0800

    added pages in views.py

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from flask import Blueprint, render_template, request, jsonify
 from doc_trans_tl import doc_trans
+from doc_trans_smt_tl import translate_smt 
 from scoring import scoring_bleu, scoring_ter
 
 views = Blueprint('views', __name__)
@@ -8,6 +9,13 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    
+    return render_template("index.html")
+# end of home()
+
+
+@views.route('/translator', methods=['GET', 'POST'])
+def translator():
     
     if request.method == 'POST':
         source = request.form.get('source_lang')
@@ -24,5 +32,27 @@ def home():
         ave_bleu = 0
         ave_ter = 0
     
-    return render_template('index.html', source = source , expected_op = expected_op, op_sen_list = op_sen_list, ave_bleu = ave_bleu, ave_ter = ave_ter)
+    return render_template('translator.html', source = source , expected_op = expected_op, op_sen_list = op_sen_list, ave_bleu = ave_bleu, ave_ter = ave_ter)
+# end of function
+
+
+@views.route('/smt_translator', methods=['GET', 'POST'])
+def smt_translator():
+    
+    if request.method == 'POST':
+        source = request.form.get('source_lang')
+        expected_op= request.form.get('expected_op')
+        dict_tl_il_result = translate_smt(source, expected_op)
+        dict_tl_il_result = pd.DataFrame(dict_tl_il_result)
+        op_sen_list = dict_tl_il_result['System Output'].tolist()
+        ave_bleu = scoring_bleu(dict_tl_il_result)
+        ave_ter = scoring_ter(dict_tl_il_result)
+    else:
+        source = ''
+        expected_op = ''
+        op_sen_list = []
+        ave_bleu = 0
+        ave_ter = 0
+    
+    return render_template('smt_translator.html', source = source , expected_op = expected_op, op_sen_list = op_sen_list, ave_bleu = ave_bleu, ave_ter = ave_ter)
 # end of function
