530df9d49ed9c44acaf9e1d0d37b40f12d5c0f73
sheyn018 <sheane39@gmail.com>
2022-12-08 20:53:21 +0800

    synced and examplebased adjustments

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from flask import Blueprint, render_template, request, jsonify
 from doc_trans_tl import doc_trans
-from doc_trans_smt_tl import smt_trans 
+from doc_trans_smt_tl import smt_trans, il_smt_trans 
 from scoring import scoring_bleu, scoring_ter
 
 views = Blueprint('views', __name__)
@@ -16,47 +16,36 @@ def home():
 
 
 @views.route('/il_tag', methods=['GET', 'POST'])
-def translator():
+def il_tag():
     
     if request.method == 'POST':
         source = request.form.get('source_lang')
-        expected_op= request.form.get('expected_op')
-        dict_tl_il_result = doc_trans(source, expected_op)
-        dict_tl_il_result = pd.DataFrame(dict_tl_il_result)
-        op_sen_list = dict_tl_il_result['System Output'].tolist()
-        ave_bleu = scoring_bleu(dict_tl_il_result)
-        ave_ter = scoring_ter(dict_tl_il_result)
+        dict_il_tl_result = il_smt_trans(source)
+        dict_il_tl_result = pd.DataFrame(dict_il_tl_result)
+        op_sen_list = dict_il_tl_result['System Output'].tolist()
+
     else:
         source = ''
-        expected_op = ''
         op_sen_list = []
-        ave_bleu = 0
-        ave_ter = 0
     
-    return render_template('il-tg_translator.html', source = source , expected_op = expected_op, op_sen_list = op_sen_list, ave_bleu = ave_bleu, ave_ter = ave_ter)
+    return render_template('il-tg_translator.html', source = source, op_sen_list = op_sen_list)
 # end of function
-
 
 @views.route('/tag_il', methods=['GET', 'POST'])
-def smt_translator():
+def tag_il():
     
     if request.method == 'POST':
         source = request.form.get('source_lang')
-        expected_op= request.form.get('expected_op')
-        dict_tl_il_result = smt_trans(source, expected_op)
+        dict_tl_il_result = smt_trans(source)
         dict_tl_il_result = pd.DataFrame(dict_tl_il_result)
         op_sen_list = dict_tl_il_result['System Output'].tolist()
-        ave_bleu = scoring_bleu(dict_tl_il_result)
-        ave_ter = scoring_ter(dict_tl_il_result)
     else:
         source = ''
-        expected_op = ''
         op_sen_list = []
-        ave_bleu = 0
-        ave_ter = 0
     
-    return render_template('tg-il_translator.html', source = source , expected_op = expected_op, op_sen_list = op_sen_list, ave_bleu = ave_bleu, ave_ter = ave_ter)
+    return render_template('tg-il_translator.html', source = source, op_sen_list = op_sen_list)
 # end of function
+
 @views.route('/home')
 def landing_page():
 
